6f2db0a66abb0b1da3975e437c4f73268f4b1b82
Shen Weizhong <swzyocowboy@icloud.com>
2014-06-13 17:25:12 +0800

    updated gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -3,97 +3,97 @@ module.exports = function (grunt) {
 	'use strict';
 	
 	grunt.initConfig({
-
+		
 		pkg: grunt.file.readJSON('package.json'),
-
+		
 		jade: {
-
+			
 			compile: {
-
+				
 				options: {
-
+					
 					pretty: true,
-
+					
 					data: {
-
+						
 						debug: false
-
+						
 					}
-
+					
 				},
-
+				
 				files: {
-
+					
 					"view/html/index.html": "view/jade/unit/index.jade"
-
+					
 				}
-
+				
 			}
-
+			
 		},
-
+		
 		htmlmin: {
-
+			
 			dist: {
-
+				
 				options: {
-
+					
 					useShortDoctype: true,
-
+					
 					removeComments: true,
-
+					
 					removeEmptyAttributes: true,
-
+					
 					collapseWhitespace: true,
-
+					
 					keepClosingSlash: false,
-
+					
 					caseSensitive: true,
-
+					
 					minifyJS: true,
-
+					
 					minifyCSS: true
-
+					
 				},
-
+				
 				files: {
-
+					
 					"view/html/index.min.html": "view/html/index.html"
-
+					
 				}
-
+				
 			}
-
+			
 		},
-
+		
 		sass: {
-
+			
 			framework: {
-
+				
 				options: {
-
+					
 					check: false,
-
+					
 					style: 'compressed',
-
+					
 					precision: 5,
-
+					
 					quiet: false,
-
+					
 					lineNumbers: true,
-
+					
 					noCache: false
-
+					
 				},
-
+				
 				files: {
-
+					
 					'public/ajax/libs/css/m.5i5dai.com_framework/m.5i5dai.com_framework.css': 'public/ajax/libs/scss/m.5i5dai.com_framework/m.5i5dai.com_framework.scss'
-
+					
 				}
-
+				
 			}
-
+			
 		},
 		
 		//TODO: CoffeeScript Lint
@@ -178,11 +178,11 @@ module.exports = function (grunt) {
 		jshint: {
 			
 			all: [
-
+				
 				'gruntfile.js',
-
+				
 				'public/ajax/libs/js/m.5i5dai.com/common.js'
-
+				
 			]
 			
 		},
@@ -202,11 +202,11 @@ module.exports = function (grunt) {
 				}
 				
 			},
-
+			
 			stylesheet: {
-
+				
 				files: ['public/ajax/libs/scss/**/*.scss'],
-
+				
 				tasks: ['sass'],
 				
 				options: {
@@ -214,13 +214,13 @@ module.exports = function (grunt) {
 					livereload: true // needed to run LiveReload
 					
 				}
-
+				
 			},
-
+			
 			coffeescript: {
-
+				
 				files: ['public/ajax/libs/coffeescript/**/*.coffee'],
-
+				
 				tasks: ['coffeelint', 'coffee', 'uglify', 'jshint'],
 				
 				options: {
@@ -228,7 +228,7 @@ module.exports = function (grunt) {
 					livereload: true // needed to run LiveReload
 					
 				}
-
+				
 			}
 			
 		}
@@ -236,17 +236,17 @@ module.exports = function (grunt) {
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jade');
-
+	
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-
+	
 	grunt.loadNpmTasks('grunt-contrib-sass');
-
+	
 	grunt.loadNpmTasks('grunt-coffeelint');
-
+	
 	grunt.loadNpmTasks('grunt-contrib-coffee');
-
+	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-
+	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	
 	grunt.loadNpmTasks('grunt-contrib-watch');
